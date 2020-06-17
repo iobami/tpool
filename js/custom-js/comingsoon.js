c7a029bcb11cccae5af034a771afd462b716221e
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-06-17 21:44:29 +0100

    created coming soon page
    created coming soon page

---
+++ js/custom-js/comingsoon.js
@@ -0,0 +1,28 @@
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 10, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for weeks,days, hours, minutes and seconds
+  var weeks = Math.floor(distance /(1000 * 60 * 60 *24 * 7))
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result 
+  countdown.innerHTML = `
+  <div>${weeks}<span>Weeks</span><div>
+  <div>${days}<span>Days</span></div> 
+  <div>${hours}<span>Hours</span></div>
+  <div>${mins}<span>Minutes</span></div>
+  <div>${seconds}<span>Seconds</span></div>
+  `;
