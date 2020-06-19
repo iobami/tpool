05e0ed91f9b2daa39843fc7efe5443a2f5f1fde2
root <root@ip-172-31-53-81.ec2.internal>
2020-06-19 11:55:32 +0000

    modified deploy.php

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@
     $commands = array(
         'echo $PWD',
         'whoami',
-        'git pull',
+       	'git pull origin develop',
         'git status',
         'git submodule sync',
         'git submodule update',
