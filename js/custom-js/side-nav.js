50a71822f63e473bbeba72e82200c15de52ca147
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-06-19 08:58:44 +0100

    feat: added side nav

--- js/custom-js/side-nav.js
+++ js/custom-js/side-nav.js
@@ -1,17 +1,23 @@
 const body = document.querySelector('body');
 const sidenavOpenButton = document.querySelector('#top__nav__toggle');
 const sidenav = document.querySelector('#sideNav');
+const content = document.querySelector('.shift')
 const sideNavCloseButton = document.querySelector('#body__click');
 
 sidenavOpenButton.addEventListener('click', () => {
     body.classList.add('body__active');
     sidenav.classList.add('sidenav__active');
+    sidenav.classList.remove('sidenav__unactive');
     sideNavCloseButton.classList.add('body__click__active');
+
+    content.classList.remove('row-cols-md-2')
 });
 
 sideNavCloseButton.addEventListener('click', () => {
-    sidenav.classList.remove('sidenav__active');
+    sidenav.classList.add('sidenav__unactive');
     sideNavCloseButton.classList.remove('body__click__active');
     body.classList.remove('body__active');
+
+    content.classList.add('row-cols-md-2')
 });
 
