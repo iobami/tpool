69c81e125a7246999141868f96fb89c6057cff98
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-06-19 12:10:33 +0100

    chore: made changes to top nav

--- js/custom-js/side-nav.js
+++ js/custom-js/side-nav.js
@@ -1,21 +1,15 @@
 const sidenavOpenButton = document.querySelector('#top__nav__toggle');
 const sidenav = document.querySelector('#sideNav');
-const content = document.querySelector('.shift')
 const sideNavCloseButton = document.querySelector('#body__click');
 
 sidenavOpenButton.addEventListener('click', () => {
     sidenav.classList.add('sidenav__active');
     sidenav.classList.remove('sidenav__unactive');
     sideNavCloseButton.classList.add('body__click__active');
-
-    content.classList.remove('row-cols-md-2')
 });
 
 sideNavCloseButton.addEventListener('click', () => {
     sidenav.classList.add('sidenav__unactive');
     sideNavCloseButton.classList.remove('body__click__active');
-    body.classList.remove('body__active');
-
-    content.classList.add('row-cols-md-2')
 });
 
