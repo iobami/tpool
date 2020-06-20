356a1570e873ccb0c86debe6c1d85f969a3ee6a2
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-06-20 22:49:53 +0100

    Bug:Countdowntimer
    Fixed bug in countdown-timer

--- js/custom-js/comingsoon.js
+++ js/custom-js/comingsoon.js
@@ -1,48 +1,29 @@
 
-  function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
-  const weeks = Math.floor(total / (1000 * 60 * 60 * 24));
   
-  return {
-    total,
-    weeks,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
-}
 
-function initializeClock(id, endtime) {
-  const clock = document.getElementById(id);
-  const weeksSpan= clock.querySelector('.weeks')
-  const daysSpan = clock.querySelector('.days');
-  const hoursSpan = clock.querySelector('.hours');
-  const minutesSpan = clock.querySelector('.minutes');
-  const secondsSpan = clock.querySelector('.seconds');
 
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+      week = day * 7
 
-    weeksSpan.innerHTML =t.weeks
-    daysSpan.innerHTML = ('0' + t.days).slice(-2);
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+let countDown = new Date('July 16, 2020 10:21:00').getTime(),
+    x = setInterval(function() {    
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
+      let now = new Date().getTime(),
+          distance = countDown - now;
 
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
-}
 
-const deadline = new Date(Date.parse(new Date()) + 16 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
-
+      document.getElementById('weeks').innerText = Math.floor(distance / (week)),
+    document.getElementById('days').innerText = Math.floor((distance % (week)) / day),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+     
+     //When date is reached, do this. 
+      if (distance < 0) {
+       clearInterval(x);
+      //insert text 
+      }
+    }, second)
