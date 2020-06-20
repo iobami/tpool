eeb2677d47aa3de0e053376c21e1d7989c824b2f
Timothy Mayor <timothymayor24@gmail.com>
2020-06-20 17:01:07 +0100

    Updated deploy.php

--- deploy.php
+++ deploy.php
@@ -10,8 +10,7 @@
     $commands = array(
         'echo $PWD',
         'whoami',
-       	'git pull',
-	#'git pull origin develop',
+       	'sudo git pull',
         'git status',
         'git submodule sync',
         'git submodule update',
