cf059be27f77ae57d41ef0d6cc8be44847798931
Osemudiamen Eronlan <oseeronlan@gmail.com>
2020-07-02 00:00:16 +0100

    fix:mobile view

--- js/custom-js/admin-dash-messages.js
+++ js/custom-js/admin-dash-messages.js
@@ -3,6 +3,9 @@ function toggleBack() {
   var messageToggle = document.getElementById("inbox-top");
   var messages = document.getElementById("right-side");
 
+  if (window.screen.width > 768) {
+    return;
+  }
   for (var i = 0; i < x.length; i++) {
     if (x[i].style.display === "none") {
       x[i].style.display = "block";
