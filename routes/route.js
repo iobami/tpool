3279009137304498e7de94e368ac11743fbab55f
Wale Timothy <awtim01@gmail.com>
2020-07-06 19:48:29 +0100

    modified the route and employee signup js file

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const appRoute = express.Router();
+const employeeSignup = require('../controllers/employee-signup');
 
 appRoute.get('/', (req, res) => {
   res.render('index', { variable: 'Hello Guys' });
 });
+
+appRoute.get('/employee-sign-up', employeeSignup.employeeSignup );
 
 module.exports = appRoute;
