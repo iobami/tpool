8719acec04b0aff227ffcfa5434809f265c9c74f
Okooboh Efosa <okoobohefosa@gmail.com>
2020-07-07 17:57:06 +0100

    add employer dashboard support link in sidebar

--- routes/employer/dashboard.js
+++ routes/employer/dashboard.js
@@ -2,13 +2,18 @@ const express = require("express");
 const appRoute = express.Router();
 
 const {
+    employerDashboard,
     employerProfile,
     employerCreateProfile,
-    employeeDashboardSettings,
+    employerDashboardSettings,
+    employerDashboardSupport,
 } = require('../../controllers/employer/dashboard');
 
+appRoute.get('/employer-dashboard', employerDashboard);
 appRoute.get('/employer-profile', employerProfile);
 appRoute.get('/employer-create-profile', employerCreateProfile);
-appRoute.get('/employer-dashboard-settings', employeeDashboardSettings);
+appRoute.get('/employer-dashboard-settings', employerDashboardSettings);
+appRoute.get('/employer-dashboard-support', employerDashboardSupport);
+
 
 module.exports = appRoute;
