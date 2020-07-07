39922eba84f061ccc65ef3ce640e5b89f158ebe2
Xemicolon <1vibenation@gmail.com>
2020-07-07 09:28:44 +0100

    Employer-SignIn

--- routes/route.js
+++ routes/route.js
@@ -3,12 +3,16 @@ const { employeeSignIn } = require('../controllers/auth');
 
 const appRoute = express.Router();
 const { employeeSignup }  = require('../controllers/employee-signup');
+const {employerSignIn} = require("../controllers/employer-signin")
 
 appRoute.get('/', (req, res) => {
   res.render('index', { pageName: 'Home' });
 });
 
-appRoute.get('/employee-sign-in', employeeSignIn);
+appRoute.get('/employer-sign-in', employerSignIn)
+
+
+appRoute.get('/employee-signin', employeeSignIn);
 appRoute.get('/employee-sign-up', employeeSignup );
 
 module.exports = appRoute;
