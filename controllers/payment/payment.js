06d2dfd1450165b4c518aaa64327c01458c81489
Xemicolon <1vibenation@gmail.com>
2020-07-07 17:10:52 +0100

    Price payment

--- controllers/payment/payment.js
+++ controllers/payment/payment.js
@@ -3,12 +3,11 @@ module.exports = {
         res.render('Pages/payment-method', {pageName: 'Payment Method'})
     },
 
-<<<<<<< HEAD
-    paymentSuccess: (req, res) => {
-        res.render('Pages/payment-success', {pageName: 'Payment Success'})
-=======
     paymentSuccessModal: (req, res) => {
         res.render('Pages/payment-success-modal', {pageName: 'Payment Success Modal'})
->>>>>>> f4380ed775c0e45c8e840497681b8f92a2c40bd7
+    },
+
+    pricePayment: (req, res) => {
+        res.render('Pages/price-payment', {pageName: 'Price Payment'})
     }
 }
--- routes/payment/index.js
+++ routes/payment/index.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const appRoute = express.Router();
 
-<<<<<<< HEAD
-const { paymentMethod, paymentSuccess } = require('../../controllers/payment/payment');
-
-appRoute.get('/payment-method', paymentMethod);
-appRoute.get('/payment-success', paymentSuccess)
-=======
-const { paymentMethod, paymentSuccessModal } = require('../../controllers/payment/payment');
+const { paymentMethod, paymentSuccessModal, pricePayment } = require('../../controllers/payment/payment');
 
 appRoute.get('/payment-method', paymentMethod);
 appRoute.get('/payment-success-modal', paymentSuccessModal);
->>>>>>> f4380ed775c0e45c8e840497681b8f92a2c40bd7
+appRoute.get('/price-payment', pricePayment);
 
 module.exports = appRoute;
 
