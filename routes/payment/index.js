4760d437bff7c6abb04aae718c462ee2524616b3
Xemicolon <1vibenation@gmail.com>
2020-07-07 17:00:31 +0100

    Payment Success Modal

--- routes/payment/index.js
+++ routes/payment/index.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const appRoute = express.Router();
 
-<<<<<<< HEAD
-const { paymentMethod, paymentSuccess } = require('../../controllers/payment/payment');
-
-appRoute.get('/payment-method', paymentMethod);
-appRoute.get('/payment-success', paymentSuccess)
-=======
 const { paymentMethod, paymentSuccessModal } = require('../../controllers/payment/payment');
 
 appRoute.get('/payment-method', paymentMethod);
 appRoute.get('/payment-success-modal', paymentSuccessModal);
->>>>>>> f4380ed775c0e45c8e840497681b8f92a2c40bd7
 
 module.exports = appRoute;
 
