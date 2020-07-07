f58fb86336b8548f81a387dec186c65893d09e1a
Xemicolon <1vibenation@gmail.com>
2020-07-07 14:41:50 +0100

    Password Reset

--- routes/password/index.js
+++ routes/password/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const appRoute = express.Router();
 
-const { passwordOTP } = require('../../controllers/password/password');
+const { passwordOTP, passwordReset } = require('../../controllers/password/password');
 
 appRoute.get('/password-otp', passwordOTP);
+appRoute.get('/password-reset', passwordReset)
 
 module.exports = appRoute;
 
