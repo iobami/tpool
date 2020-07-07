a6556d11fc1deb046d6a0a754f75d5847f4f6512
Wale Timothy <awtim01@gmail.com>
2020-07-07 07:44:08 +0100

    fixed routing

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const appRoute = express.Router();
-const employeeSignup = require('../controllers/employee-signup');
+const { employeeSignup }  = require('../controllers/employee-signup');
 
 appRoute.get('/', (req, res) => {
   res.render('index', { pageName: 'Home' });
 });
 
-appRoute.get('/employee-sign-up', employeeSignup.employeeSignup );
+appRoute.get('/employee-sign-up', employeeSignup );
 
 module.exports = appRoute;
