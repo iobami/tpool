49a6af6c51162816a66fb205ae24acd780e09c25
Orefejo <adepeju.kayode@gmail.com>
2020-07-09 15:45:16 +0200

    chore: user logout

--- public/js/custom-js/logout.js
+++ public/js/custom-js/logout.js
@@ -1,5 +1,18 @@
-function logOutFunction() {
+async function logoutFunction(fromLogoutPage = false) {
     localStorage.clear();
+    if (!fromLogoutPage) {
+        let status = 'unsuccessful';
+        let message;
+        try {
+            const { data } = await axios.get('https://api.lancers.app/v1/auth/logout');
+            status = data.status;
+            message = data.data;
+        } catch (e) {
+            message = 'Logout unsuccessful'
+        }
+        document.getElementById('logout-toast').classList.add(status);
+        document.getElementById('logout-toast-body').innerHTML = message;
+    }
     $('.toast').toast('show');
     // localStorage.setItem('tpAuth', '');
     window.location.replace('/');
