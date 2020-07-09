6e0a01921b055d0a168228c1da96dcf66bb17195
Isaac Sokari <isaacsokari@gmail.com>
2020-07-09 15:59:00 +0100

    Revert "Forgot password API task (#425)" (#471)
    This reverts commit 31e5dff343b126bbd84496534093d4d531aa27f1.

--- public/js/custom-js/forgot-password.js
+++ public/js/custom-js/forgot-password.js
@@ -1,44 +1,17 @@
 const forgotPasswordForm = document.querySelector('#forgot-password');
 
-// Function to show alert message
-function showMessage(message, alertType) {
-	const alert = document.createElement("div");
-	alert.id = "parentdiv";
-
-	alert.innerHTML = `
- <div class="alert ${alertType}" role="alert" id="error">
-   ${message}
- </div>
- `;
-	forgotPasswordForm.insertBefore(alert, forgotPasswordForm.firstElementChild);
-	setTimeout(removeMessage, 5000);
-}
-
-// Function to remove alert message
-function removeMessage() {
-	let message = document.getElementById("parentdiv");
-	message.remove();
-}
-
-// Function to handle response from API
-function responseHandler(res) {
-	if (res.data.status === "success") {
-		showMessage(`Success, ${res.data.data.message}...`, "alert-success");
-    }
-}
-function errorHandler(err) {
-	showMessage("Error, User not found!", "alert-danger");
-}
-
 forgotPasswordForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    forgotPasswordForm.value="";
     const email = forgotPasswordForm.querySelector('input').value;
 
-    axios.post('https://api.lancers.app/v1/auth/forgot-password', {
-        email: email
+    fetch('https://api.lancers.app/v1/auth/forgot-password', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "User-Agent": "Developers Lancers",
+        },
+        body: JSON.stringify({email})
     })
-        .then(res => responseHandler(res))
-        .catch(err => errorHandler(err))
-        document.getElementById("forgot-password").reset();
+        .then( res => res.json() )
+        .then(data => console.log(data))
 })
