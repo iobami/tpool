3bed915e1ff0eb57851daa794c4740a018379b36
sahmie <sahmienwoko@gmail.com>
2020-07-14 11:00:32 +0100

    add employee and employer handler for github

--- Routes/googleAuth.js
+++ Routes/googleAuth.js
@@ -6,6 +6,7 @@ router.get('/auth/dashboard', (req, res) => {
   res.json(req.user);
 });
 
+// <----------------------- GOOOGLE ROUTE AND CONTOLLERS ------------------------------>
 // get employer profile details from google
 router.get('/auth/employer/google',
   passport.authenticate('google-employer', { scope: ['profile', 'email'] }));
@@ -45,5 +46,74 @@ router.get('/auth/employee/google/callback',
     req.session.userId = user.user_id;
     return res.redirect('/employee/dashboard');
   });
+// <===================== END GOOGLE ===================>
 
+// ------------------------- GITHUB ROUTES AND CONTROLLERS ---------------------->
+// get employer profile details from github
+router.get('/auth/employer/github',
+  passport.authenticate('github-employer'));
+
+// receive process details from passport.setup
+router.get('/auth/github/callback',
+  passport.authenticate('github-employer', { failureRedirect: '/employer-sign-in' }),
+  (req, res) => {
+    try {
+      const { user } = req;
+      if (user.userRole === 'ROL-EMPLOYER') {
+        if ((!user.userTypeId) || user.userTypeId == null) {
+          req.session.isLoggedIn = false;
+          req.session.userId = user.user_id;
+          return res.redirect('/employer-create-profile');
+        }
+        req.session.isLoggedIn = true;
+        req.session.userId = user.user_id;
+        return res.redirect('/employer-dashboard');
+      }
+      if ((!user.userTypeId) || user.userTypeId == null) {
+        req.session.isLoggedIn = false;
+        req.session.userId = user.user_id;
+        return res.redirect('/employee/profile/create');
+      }
+      req.session.isLoggedIn = true;
+      req.session.userId = user.user_id;
+      return res.redirect('/employee/dashboard/:employee_id');
+    } catch (error) {
+      res.redirect('/employer-sign-in');
+    }
+  });
+
+// get employee profile details from github
+router.get('/auth/employee/github',
+  passport.authenticate('github-employee'));
+
+// receive process details from passport.setup
+router.get('/auth/github/callback',
+  passport.authenticate('github-employee', { failureRedirect: '/employee/login' }),
+  (req, res) => {
+    try {
+      const { user } = req;
+      if (user.userRole === 'ROL-EMPLOYER') {
+        if ((!user.userTypeId) || user.userTypeId == null) {
+          req.session.isLoggedIn = false;
+          req.session.userId = user.user_id;
+          return res.redirect('/employer-create-profile');
+        }
+        req.session.isLoggedIn = true;
+        req.session.userId = user.user_id;
+        return res.redirect('/employer-dashboard');
+      }
+      if ((!user.userTypeId) || user.userTypeId == null) {
+        req.session.isLoggedIn = false;
+        req.session.userId = user.user_id;
+        return res.redirect('/employee/profile/create');
+      }
+      req.session.isLoggedIn = true;
+      req.session.userId = user.user_id;
+      return res.redirect('/employee/dashboard/:employee_id');
+    } catch (error) {
+      res.redirect('/employee/login');
+    }
+  });
+
+// <======================== END GITHUB =========================>
 module.exports = router;
--- config/passport.setup.js
+++ config/passport.setup.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const { uuid } = require('uuidv4');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
 const model = require('../Models/index');
 const jsonWT = require('../Utils/auth-token');
 const { getUserData, createUser } = require('../Utils/helpers/passport-helper');
@@ -22,7 +23,7 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-// employer auth
+// employer google auth
 passport.use('google-employer',
   new GoogleStrategy(
     {
@@ -55,13 +56,79 @@ passport.use('google-employer',
     },
   ));
 
-// employee authentication
+// employee google authentication
 passport.use('google-employee',
   new GoogleStrategy(
     {
       clientID: process.env.TALENT_POOL_GOOGLE_CLIENTID,
       clientSecret: process.env.TALENT_POOL_GOOGLE_CLIENTSECRET,
       callbackURL: process.env.TALENT_POOL_GOOGLE_EMPLOYEE_CALLBACKURL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // check user in our db
+        const checkUser = await model.User.findOne({
+          where: {
+            email: profile.emails[0].value,
+          },
+        });
+        const user = await checkUser;
+        if (user) {
+          // user exists, send user object for serialization
+          const data = await getUserData(profile, user, done);
+          done(null, data);
+        } else {
+          // create a new user
+          const userData = await createUser(profile, 'ROL-EMPLOYEE');
+
+          return done(null, userData);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  ));
+
+// employer github auth
+passport.use('github-employer',
+  new GitHubStrategy(
+    {
+      clientID: process.env.TALENT_POOL_GITHUB_CLIENTID,
+      clientSecret: process.env.TALENT_POOL_GITHUB_CLIENTSECRET,
+      callbackURL: process.env.TALENT_POOL_GITHUB_CALLBACKURL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // check user in our db
+        const checkUser = await model.User.findOne({
+          where: {
+            email: profile.emails[0].value,
+          },
+        });
+        const user = await checkUser;
+        if (user) {
+          // user exists, send user object for serialization
+          const data = await getUserData(profile, user, done);
+          done(null, data);
+        } else {
+          // create a new user
+          const userData = await createUser(profile, 'ROL-EMPLOYER');
+
+          return done(null, userData);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  ));
+
+// employee github authentication
+passport.use('github-employee',
+  new GitHubStrategy(
+    {
+      clientID: process.env.TALENT_POOL_GITHUB_CLIENTID,
+      clientSecret: process.env.TALENT_POOL_GITHUB_CLIENTSECRET,
+      callbackURL: process.env.TALENT_POOL_GITHUB_CALLBACKURL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
